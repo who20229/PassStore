9fe491b6b4190f1eb4a43b44b49475c57311a36f
idk <80971614+who20229@users.noreply.github.com>
2023-11-25 14:32:16 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,136 @@
+import os
+import re
+import sys
+import tabulate
+import requests
+from termcolor import colored
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from password_generator import PasswordGenerator
+load_dotenv()
+ConnectionString = os.environ["CONNECTION"] 
+mClient = MongoClient(ConnectionString)
+db = mClient['Softaim']
+userCollection = db['finalUserSchema']
+passCollection = db['pasSchema']
+
+def main():
+    username = login()
+    commands = [["1", "View", "View saved password"], ["2", "Save", "Save new password"], ["3", "Rate", "Check how strong your password is"], 
+                ["4", "Suggest", "Suggest changes to developer"], ["5", "Generate", "Generate a strong pasword"], ["6", "Delete", "Delete Saved Password"]]
+    allowed = ["1", "2", "3", "4", "5", "6", "one", "two", "three", "four", "five", "six"]
+    headers = {"Command", "Function", "Description"}
+    print("+-----------+-----------------+\nPlease type the command to be executed:", end="\n\n")
+    print(tabulate.tabulate(commands, headers, tablefmt="grid"))
+    cmd = input(">> ").strip().lower()
+    if cmd in allowed:
+        if cmd == "5" or cmd == "five":
+            print(f"Suggested password: {suggest()}")
+            yn2 = input("would you like the program to suggest you another strong password? (y/n)").strip().lower()
+            if yn2 == 'y' or yn2 == 'yes':
+                print(f"Suggested password: {suggest()}")
+            elif yn2 == 'n' or yn2 == "no":
+                sys.exit("Stopping the program")
+            else:
+                print(colored("Please provide a valid argument (y, n, yes, no)", "red"))
+        elif cmd == "3" or cmd == "three":
+            toRate = input(colored("Enter your password: ", "yellow")).strip()
+            security = rate(toRate)
+            print(f"Your accounts password is {security}")
+            if "weak" in security:
+                yn = input("would you like the program to suggest you a strong password? (y/n)").strip().lower()
+                if yn == 'y' or yn == 'yes':
+                    print(f"Suggested password: {suggest()}")
+                elif yn == 'n' or yn== "no":
+                    sys.exit("Stopping the program")
+        elif cmd == '1' or cmd == "one":
+            app = input("password for? (App/website name): ").upper().strip()
+            table = view(username, app)
+            print(table)
+        elif cmd == '2' or cmd == "two":
+            app = input("App/website Name: ").strip()
+            passw = input("App Password: ").strip()
+            print(set(username,app, passw))
+        elif cmd == '6'  or cmd == "six":
+            print(delete(username))
+        elif cmd == '4' or cmd == "four":
+            suggestion = input("Enter suggestion: ").strip()
+            print(suggestDEV(suggestion))
+        else:
+            print(colored("Please provide a valid argument (y, n, yes, no)", "red"))
+    else:
+        sys.exit(colored("Please provide a valid input (command number from the table)", "red"))
+
+def view(name, app):
+    if found := passCollection.find_one({"username": name, "name": app}):
+        info = [[app.capitalize(), found["password"]]]
+        theaders = ["App", "Password"]
+        return tabulate.tabulate(info, theaders, tablefmt="grid")
+    else:
+        sys.exit(colored("The data was not found, use [2] save command to save it.", "red"))
+
+def set(username, app, passw):
+    passCollection.insert_one({
+        "username":username,
+        "name":app.upper(),
+        "password":passw
+    })
+    return colored("Data saved", "green")
+
+def delete(name):
+    app = input("App/website Name: ").strip().upper()
+    if found := passCollection.find_one({"username": name, "name": app}):
+        passCollection.find_one_and_delete({
+            "username":name,
+            "name":app.upper(),
+        })
+        return colored("Data removed", "green")
+    else:
+        return colored(f"Password for {app.capitalize()} does not exist", "red")
+def suggest():
+    pwo = PasswordGenerator()
+    password = pwo.generate()
+    return password
+
+def rate(toRate):
+    if strong := re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$", toRate):
+        return colored("strong", "green")
+    else:
+        return colored("[!] weak: combine symbols, numbers and letters in different case.", "red")
+    
+def login():
+    print("+-----------+-----------------+\nEnter username & password")
+    pUser = input("USERNAME: ").strip()
+    pWord = input("PASSWORD: ").strip()
+    if check := userCollection.find_one({'username':pUser, 'password': pWord}):
+         print(colored(f"Logged in as {pUser}", "green"))
+         return pUser
+    else:
+        print("We couldn't find your data:")
+        setup()
+
+def setup():
+    print("+-----------+-----------------+\nSetup username & password")
+    pUser = input("USERNAME: ").strip()
+    pWord = input("PASSWORD: ").strip()
+    if check := userCollection.find_one({'username':pUser}):
+        sys.exit(f"Please try again with another username as {pUser} is taken.")
+    else:
+        strong = re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$", pWord)
+        userCollection.insert_one({
+            "username":pUser,
+            "password":pWord
+        })
+        print(colored("Your data is saved, you can now login!", "green"))
+    if not strong:
+        print("Your password can be improved, you can change it (using change command) to secured", end=" ")
+        print(colored(f"suggested password: {suggest()}", "yellow"))
+    login()
+
+def suggestDEV(sug):
+    DISCORD_WEBHOOK_URL = os.environ['DISCORD_WEBHOOK_URL']
+    payload = {'content': f"New suggestion for CS50P project: {sug}"}
+    response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
+    return "your suggestion has been " + colored("sent!", "green")
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,78 @@
+# [📍] About this project : PassStore
+This project is a program that can be helpful for people who often forget their password! The user can: Save or delete a password of a specific app which is saved in a mongodb database, validate that their password is strong,
+get the program to suggest a strong password.However, as no form of encryption is used, I do not suggest connection to database that another person owns.  
+I made this project as my final project of CS50, if you have any questions, please send me a request in `late.np` on Discord.
+## [⌨️] USAGE: How do i use this?
+`requirements.txt` holds all the required libraries, it can be installed with `pip install -r requirements.txt`  
+
+in .env file  
+```
+DISCORD_WEBHOOK_URL=Your_discord_webhook_url
+CONNECTION=MONGO_CONNECTION_URL
+```
+replace `MONGO_CONNECTION_URL` with your connection string ([How to get mongo connection URL](https://www.mongodb.com/basics/mongodb-connection-string#:~:text=The%20MongoDB%20connection%20string%20for,port%20number%20you%20are%20using.))  
+replace `DISCORD_WEBHOOK-URL` with your webhook URL
+  
+in `main.py` go to line 13 
+```py
+db = mClient['DB_NAME']
+```
+replace `DB_NAME` with the name of your database  
+  
+after installation:  
+run: `python main.py` and enter a username and password
+```
++-----------+-----------------+
+Enter username & password
+USERNAME: Your_username
+PASSWORD: Your_password
+```
+if you are using this for the first time, you will be asked to register!
+```
+Enter username & password
+USERNAME: Your_username
+PASSWORD: Your_password
+We couldn't find your data:
++-----------+-----------------+
+Setup username & password
+USERNAME: new_username
+PASSWORD: new_password
+```
+after your login with your newly created username, you will be presented with a table of cmds select any one of them and follow the instructions
+```
++-----------+-----------------+
+Please type the command to be executed:
+
++------------+---------------+-----------------------------------+
+|   Function | Description   | Command                           |
++============+===============+===================================+
+|          1 | View          | View saved password               |
++------------+---------------+-----------------------------------+
+|          2 | Save          | Save new password                 |
++------------+---------------+-----------------------------------+
+|          3 | Rate          | Check how strong your password is |
++------------+---------------+-----------------------------------+
+|          4 | Suggest       | Suggest changes to developer      |
++------------+---------------+-----------------------------------+
+|          5 | Generate      | Generate a strong pasword         |
++------------+---------------+-----------------------------------+
+|          6 | Delete        | Delete Saved Password             |
++------------+---------------+-----------------------------------+
+>>
+```
+## [❓] Requirements and its functions
+os: to access .env   
+re : to check the strength of function  
+sys : to access command line arguments  
+tabulate : to showcase the command options and retrieved password when requested  
+requests : send suggestions to dev
+termcolor : aesthetics  
+dotenv :access .env  
+pymongo: database  
+password_generator: generate random passwords
+
+## [😲] What can be expected in the future
+I plan on encrypting the password and usernames of user and store it in the database, this will only be decrypted when the user requests to view it so it can be more secure. The password right now is not encrypted before being saved to [mongodb](https://www.mongodb.com). So i suggest to use this in your **own** db only
+
+
+## To know more refer to ths [Video Tutorial](https://youtu.be/ZFkeogXgRf4)
